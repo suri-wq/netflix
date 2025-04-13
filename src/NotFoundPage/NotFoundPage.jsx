1826aa7654ccd78c1f3cbbd6bfaf0d4c3ad02f60
Yiseul Kwon <yiseul@Yiseuls-Mac-mini.local>
2025-04-13 21:56:21 +0900

    change background color

--- src/NotFoundPage/NotFoundPage.jsx
+++ src/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import "./NotFoundPage.style.css"
+import { Link } from 'react-router-dom'
 const NotFoundPage = () => {
   return (
     <Container className='not-found-container'>
         <div>
             <h1>😭</h1>
             <h2>This is not the page you are looking for</h2>
-            <h3>Wanna go home? 👉</h3>
+            <h3>Wanna go home? 👉
+                <Link to='/'>
+                     🏡
+                </Link>
+            </h3>
+            
         </div>
         
 
