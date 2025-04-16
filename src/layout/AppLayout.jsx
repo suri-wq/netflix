3ef00e80c207d41458cf7313f7ab0a8ed8dabd0a
Yiseul Kwon <yiseul@Yiseuls-Mac-mini.local>
2025-04-17 00:59:38 +0900

    페이지네이션 추가

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import './AppLayout.style.css'
 const AppLayout = () => {
+    const [keyword, setKeyword] = useState('')
+    const navigate = useNavigate()
+    const searchByKeyword=(event)=>{
+        event.preventDefault()
+        // url을 바꿔주기
+        navigate(`/movies?q=${keyword}`);
+        setKeyword('')
+    }
   return (
     <div>
         <Navbar expand="lg" className="nav-body">
@@ -40,14 +47,16 @@ const AppLayout = () => {
                     Link
                     </Nav.Link> */}
                 </Nav>
-                <Form className="d-flex">
+                <Form className="d-flex" onSubmit={searchByKeyword}>
                     <Form.Control
                     type="search"
                     placeholder="Search"
                     className="me-2"
                     aria-label="Search"
+                    value={keyword}
+                    onChange={(event)=>setKeyword(event.target.value)}
                     />
-                    <Button variant="outline-danger">Search</Button>
+                    <Button variant="outline-danger" type="submit">Search</Button>
                 </Form>
                 </Navbar.Collapse>
             </Container>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchMoviQuery } from '../../hooks/useSearchMovie'
+import { useSearchParams } from 'react-router-dom'
+import api from '../../utils/api'
+import { Alert, Container, Spinner, Col, Row } from 'react-bootstrap'
+import MovieCard from '../../common/MovieCard/MovieCard'
+import ReactPaginate from 'react-paginate'
+
 
 const MoviePage = () => {
+  const [query, setQuery] = useSearchParams()
+  const [page, setPage] = useState(1)
+  const keyword = query.get('q');
+
+  const {data, isLoading, isError, error} = useSearchMoviQuery({keyword,page})
+  const handlePageClick=({selected})=>{
+    console.log("page",page)
+    setPage(selected+1)
+  }
+  
+  console.log("ddd",data)
+  if (isLoading){
+    return(
+      <div>
+        <Spinner
+          animation='border'
+          variant='danger'
+          style={{ width: "5rem", height:"5rem"}}
+        />
+      </div>
+    )
+  }
+
+  if (isError){
+    return <Alert variant='danger'>{error.message}</Alert>
+  }
+  
   return (
-    <div>MoviePage</div>
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}>필터</Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie,index)=><Col key={index} lg={4} xs={12}>
+            <MovieCard movie={movie}/>
+          </Col>)}
+          </Row>
+        </Col>
+          <ReactPaginate
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={data?.total_pages}
+          previousLabel="< previous"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+          renderOnZeroPageCount={null}
+          forcePage={page-1}
+          />
+      </Row>
+
+    </Container>
   )
 }
 
